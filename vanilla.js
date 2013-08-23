34076273a5e294d99c8ef2730a2e030df47d683c
Colyn Brown <colyn.brown@gmail.com>
2013-08-23 10:22:12 -0700

    initial commit

---
+++ vanilla.js
@@ -0,0 +1,10 @@
+/*!
+ * Vanilla.js JavaScript Library v0.1.0
+ * http://colynb.com/vanilla.js/
+ *
+ * Copyright 2011, 2012, 2013 Colyn Brown
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2013-08-20T13:48Z
+ */
